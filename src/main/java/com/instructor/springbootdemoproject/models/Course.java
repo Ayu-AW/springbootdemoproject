0c899fd66f045970fe7a05767eaa7b50ff027c94
Jafer Alhaboubi <57419878+jhabboubi@users.noreply.github.com>
2022-06-04 14:26:15 -0500

    initialize Student.java and Course.java create association for models

--- src/main/java/com/instructor/springbootdemoproject/models/Course.java
+++ src/main/java/com/instructor/springbootdemoproject/models/Course.java
@@ -1,4 +1,55 @@
 package com.instructor.springbootdemoproject.models;
 
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author jafer alhaoubi
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter @Setter @Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "course")
+@Entity
 public class Course {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @NonNull
+    String name;
+    @NonNull
+    String instructor;
+
+    @ManyToMany(mappedBy = "courses", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    Set<Student> students = new LinkedHashSet<>();
+
+    // Helper Method
+    public void addStudent(Student student){
+        students.add(student);
+        student.getCourses().add(this);
+    }
+
+    // Hashcode and Equals override
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && name.equals(course.name) && instructor.equals(course.instructor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, instructor);
+    }
 }
--- src/main/java/com/instructor/springbootdemoproject/models/Student.java
+++ src/main/java/com/instructor/springbootdemoproject/models/Student.java
@@ -1,4 +1,62 @@
 package com.instructor.springbootdemoproject.models;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author jafer alhaoubi
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter @Setter @Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "student")
+@Entity
 public class Student {
+
+    // Fields
+
+    @Id @NonNull
+    String email;
+    @NonNull
+    String name;
+    @NonNull
+    String password;
+
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinTable(name = "student_courses",
+            joinColumns = @JoinColumn(name = "student_email"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
+    Set<Course> courses = new LinkedHashSet<>();
+
+    // Helper Method
+    public void addCourse(Course course){
+        courses.add(course);
+        course.getStudents().add(this);
+    }
+
+    // Hashcode and Equals override
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return email.equals(student.email) && name.equals(student.name) && password.equals(student.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, name, password);
+    }
 }
