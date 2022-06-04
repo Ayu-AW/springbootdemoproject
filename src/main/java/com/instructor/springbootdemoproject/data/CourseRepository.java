bd90b72ea3ca4965081612fa1339a660235945ed
Jafer Alhaboubi <57419878+jhabboubi@users.noreply.github.com>
2022-06-04 14:34:19 -0500

    data/dao wired to use Spring Data

--- src/main/java/com/instructor/springbootdemoproject/data/CourseRepository.java
+++ src/main/java/com/instructor/springbootdemoproject/data/CourseRepository.java
@@ -1,4 +1,9 @@
 package com.instructor.springbootdemoproject.data;
 
-public interface CourseRepository {
+import com.instructor.springbootdemoproject.models.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Integer> {
 }
--- src/main/java/com/instructor/springbootdemoproject/data/StudentRepository.java
+++ src/main/java/com/instructor/springbootdemoproject/data/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.instructor.springbootdemoproject.data;
 
-public interface StudentRepository {
+import com.instructor.springbootdemoproject.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
 }
